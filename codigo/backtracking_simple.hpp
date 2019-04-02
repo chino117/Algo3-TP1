c05c1159322c96ecb67aa66630b769bf8e14c3e6
Emiliano <eslucero2010@gmail.com>
2019-04-02 19:11:04 -0300

    Arregle merge 2

--- codigo/backtracking_simple.hpp
+++ codigo/backtracking_simple.hpp
@@ -9,14 +9,13 @@ void Solve_Backtracking_Simple(const ConjLineal& s, const DatosProblema& dp, Con
     unsigned int b_nodo = beneficio_conj(candidato, dp);
     cout<<"Nodo: "<<nodos<<"| Beneficio: "<<b_nodo<<endl; 
     if (b_nodo > sol && tamanio_conj(candidato, dp) <= dp.W)
-        sol = beneficio_conj(candidato, dp);
+        sol = b_nodo;
 
     for(unsigned int i = idx_elem; i < s.size();i++){
         candidato.push_back(s[i]);
         Solve_Backtracking_Simple(s, dp, candidato, sol, i+1, nodos);
         candidato.pop_back();
     }
-    return;
 }
 
 ResultadoProblema Backtracking_Simple(const ConjLineal& s, const DatosProblema& dp){
